a717541fabc9035a494661b48dadb36a4d723e23
rolies106 <rolies106@gmail.com>
2014-12-07 05:36:57 +0700

    Fix #25 add find report by radius

--- fuel/app/classes/model/report.php
+++ fuel/app/classes/model/report.php
@@ -25,4 +25,35 @@ class Model_Report extends \Orm\Model
         return $val;
     }
 
+    /**
+     * Find same report within radius
+     * @param  object  $record Report record object model
+     * @param  integer $radius Radius by km
+     * @return object
+     */
+    public function findByRadius($record, $radius = 10)
+    {
+        $latitude = $record->latitude;
+        $longitude = $record->longitude;
+
+        $query = 'SELECT 
+                    id,
+                    event_id,
+                    longitude,
+                    latitude,
+                    ACOS( SIN( RADIANS( `latitude` ) ) * SIN( RADIANS( :latitude ) ) + COS( RADIANS( `latitude` ) )
+                    * COS( RADIANS(:latitude)) * COS( RADIANS( `longitude` ) - RADIANS(:longitude)) ) * 6380 AS `distance`
+                    FROM `reports`
+                    WHERE
+                    ACOS( SIN( RADIANS( `latitude` ) ) * SIN( RADIANS(:latitude) ) + COS( RADIANS( `latitude` ) )
+                    * COS( RADIANS(:latitude)) * COS( RADIANS( `longitude` ) - RADIANS(:longitude)) ) * 6380 < :radius
+                    ORDER BY `distance`';
+
+        $reports = DB::query($query)->param('longitude', $longitude)
+                                  ->param('latitude', $latitude)
+                                  ->param('radius', $radius)
+                                  ->execute();
+
+        return $reports;
+    }
 }
