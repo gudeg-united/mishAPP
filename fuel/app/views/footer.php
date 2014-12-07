5fa1f335b9fa548c2071bb95c664e8fc7570db68
Rifqi Fachriza <fachriza@outlook.com>
2014-12-08 02:17:52 +0700

    update route for static page

--- fuel/app/views/footer.php
+++ fuel/app/views/footer.php
@@ -1,16 +1,22 @@
 <footer>
   <ul>
     <li>
+      <a href="/">Home</a>
+    </li>
+    <li>
       <a href="#">Latest disasters at my location</a>
     </li>
     <li>
       <a href="#">Survival tips</a>
     </li>
     <li>
-      <a href="#">Missing people</a>
+      <a href="<?php echo Uri::create('missing'); ?>">Missing people</a>
     </li>
     <li>
-      <a href="#">Food supplies</a>
+      <a href="<?php echo Uri::create('supplies'); ?>">Food supplies</a>
+    </li>
+    <li>
+      <a href="<?php echo Uri::create('about'); ?>">About</a>
     </li>
   </ul>
 </footer>
