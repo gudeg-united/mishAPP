b4d7ed89fa0973ebcf4acb448d2340fcddd41cab
Rifqi Fachriza <fachriza@outlook.com>
2014-12-08 02:15:56 +0700

    add static page food supplies

---
+++ fuel/app/views/welcome/supplies.php
@@ -0,0 +1,9 @@
+<div class="row">
+  <div class="columns">
+    <div id="logo">
+      <img src="/assets/img/logo.png" />
+    </div>
+    <h1 class="text-center">Food Supplies</h1>
+    <p class="text-center">Coming Soon</p>
+  </div>
+</div>
