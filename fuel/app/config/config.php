a939f34b1387da947e5a8798c0fbbc9ca990b387
Arman Adhitama <blackbiron@Armans-MacBook.local>
2014-12-08 06:16:31 +0700

    fix some migration

--- fuel/app/config/config.php
+++ fuel/app/config/config.php
@@ -92,7 +92,7 @@ return array(
      * default_timezone     optional, if you want to change the server's default timezone
      */
     // 'server_gmt_offset'  => 0,
-    // 'default_timezone'   => null,
+    'default_timezone'   => 'Asia/Jakarta',
 
     /**
      * Logging Threshold.  Can be set to any of the following:
---
+++ fuel/app/migrations/021_alter_events.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Fuel\Migrations;
+
+class Alter_events
+{
+	public function up()
+	{
+		\DBUtil::modify_fields('events', array(
+        'created_at' => array('type' => 'timestamp', 'default' => \DB::expr('CURRENT_TIMESTAMP')),
+        'updated_at' => array('type' => 'timestamp', 'default' => \DB::expr('CURRENT_TIMESTAMP')),
+    ));
+	}
+
+	public function down()
+	{
+		\DBUtil::modify_fields('events', array(
+        'created_at' => array('constraint' => 11, 'type' => 'int', 'null' => true),
+        'updated_at' => array('constraint' => 11, 'type' => 'int', 'null' => true),
+    ));
+	}
+}
