59d7d9693b12aeca58cb94fbba579570bf60b253
Riza Arsyi <riza.arsyi@gmail.com>
2014-12-08 03:33:50 +0700

    map global disaster

--- fuel/app/classes/controller/maps.php
+++ fuel/app/classes/controller/maps.php
@@ -1,13 +1,23 @@
 <?php
 
-class Controller_Maps extends Controller_Template
+class Controller_Maps extends Controller_Base
 {
-
-	public function action_index()
+    public function action_index($page=1)
 	{
-		$data["subnav"] = array('index'=> 'active' );
-		$this->template->title = 'Maps &raquo; Index';
-		$this->template->content = View::forge('maps/index', $data);
-	}
 
+        $result = Requests::get('http://127.0.0.1:5000/disasters?page=' . $page . '&per_page=10');
+        $data['disaster'] = json_decode($result->body);
+
+        $pagination = Pagination::forge('mypagination', array(
+            'pagination_url' => null,
+            'uri_segment' => 3,
+            'total_items' => $data['disaster']->meta->total,
+            'per_page' => $data['disaster']->meta->per_page,
+        ));
+
+
+
+		$this->template->title = 'Maps &raquo; Index';
+		$this->template->content = View::forge('maps/index', $data );
+	}
 }
