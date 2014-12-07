7eb04af54e2b1b8cdcf319b896c489de3f478719
Rifqi Fachriza <fachriza@outlook.com>
2014-12-07 21:35:16 +0700

    update themes and assets

--- fuel/app/views/welcome/index.php
+++ fuel/app/views/welcome/index.php
@@ -1,22 +1,21 @@
 <div id="logo">
-    <img src="/assets/img/logo.png" />
+  <img src="/assets/img/logo.png" />
 </div>
 <h1 class="text-center">
-    Report a disaster
+  Report a disaster
 </h1>
 <ul class="submit-btns" id="buttons">
-    <input type="hidden" value="" id="latitude">
-    <input type="hidden" value="" id="longitude">    
-    <?php if (!empty($buttons)) : ?>
-        <?php foreach ($buttons as $button) : ?>
-            <a title="<?php echo $button->name; ?>" class="enabled report-disaster"
-                href="<?php echo Uri::create('report/disaster', array('type' => $button->id), array('type' => ':type')); ?>">
-                <?php echo $button->name; ?>
-                <img src="/assets/img/icon-earthquake.png" />
-            </a>        
-        <?php endforeach; ?>
-    <?php endif; ?>
+  <input type="hidden" value="" id="latitude">
+  <input type="hidden" value="" id="longitude">    
+  <?php if (!empty($buttons)) : ?>
+  <?php foreach ($buttons as $button) : ?>
+  <li><a title="<?php echo $button->name; ?>" class="disabled report-disaster"
+         href="<?php echo Uri::create('report/disaster', array('type' => $button->id), array('type' => ':type')); ?>">
+      <img src="/assets/img/icon-earthquake.png" />
+  </a></li>        
+  <?php endforeach; ?>
+  <?php endif; ?>
 </ul>
 <ul class="submit-btns" id="messageButtons">
-    <li><p>Getting your location.</p></li>
+  <li><p>Getting your location.</p></li>
 </ul>
