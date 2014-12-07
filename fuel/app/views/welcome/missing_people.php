cb2b87c912a2a9df667a2f24f4a377464d3e7a89
Rifqi Fachriza <fachriza@outlook.com>
2014-12-08 02:16:30 +0700

    add static page about

---
+++ fuel/app/views/welcome/missing_people.php
@@ -0,0 +1,9 @@
+<div class="row">
+  <div class="columns">
+    <div id="logo">
+      <img src="/assets/img/logo.png" />
+    </div>
+    <h1 class="text-center">Missing People</h1>
+    <p class="text-center">Coming Soon</p>
+  </div>
+</div>
